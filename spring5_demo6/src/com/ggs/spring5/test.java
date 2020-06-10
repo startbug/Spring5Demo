08e58bf2477c7b36737568bf029a9f4f6ce1f0bb
tom_glb <goodMorning_glb@ggs.com>
2020-06-10 20:09:12 +0800

    修改了gitignore内容,增加了过滤条件

---
+++ spring5_demo6/src/com/ggs/spring5/test.java
@@ -0,0 +1,13 @@
+package com.ggs.spring5;
+
+/**
+ * @author: Starbug
+ * @date: 2020/6/9 0:05
+ */
+public class test {
+    public static void main(String[] args) {
+
+    System.out.println("ssfsd");
+        System.out.println("xxx");
+    }
+}
